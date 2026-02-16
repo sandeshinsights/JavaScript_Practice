4b4f27cfeed5d3816faf81dfad7376442f3e321c
Sandesh Bhandari <sandeshbhandari428@gmail.com>
2026-02-16 23:56:20 +0000

    variables and data types

---
+++ basics/1_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = 'sandesh@google.com'
+var accountPassword = '12345'
+accountCity = 'Urlabari' 
+
+// accountId = 2 // not allowed
+
+accountEmail = 'hc@hc.com'
+accountPassword = '21212121'
+accountCity = 'Bengaluru'
+
+console.log(accountId)
+
+
+/*
+Prefer not to use var
+because of issue in block  scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ basics/2_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 
+    3           //code readability that should be high -> don't do like this 
+)
+
+console.log('Sandesh')
+
+
+let name = 'Sandesh'
+let age = 18 
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ''
+// boolean => ture/false
+// null => standalone value
+// undefined => 
+// symbol =>unique
+
+//object 
+
+console.log(typeof age)
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);; //object
